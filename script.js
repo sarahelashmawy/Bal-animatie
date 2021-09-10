9ed0c26053f90960823bb51203e75d9387de2d39
Sarah Elashmawy <74767392+sarahelashmawy@users.noreply.github.com>
2021-09-10 08:29:49 +0000

    1 ball stuitert

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-var x, y, v;
+var x, y, vx, vy;
 
 function setup() {
-  createCanvas(500, 400);
+  createCanvas(500, 500);
 
   x = 0;
-  y = 200
-  v = 5;
+  y = 200;
+  vx = 5;
+  vy = 2;
 }
 
 function draw() {
   background(225);
+ 
+  ellipse(x, y, 50, 50)
+  x = x + vx;
+  y = y + vy;
 
-  ellipse(x,y,50,50);
-  x = x + v;
-  y = y + -v;
 
   if(x <= 0 || x  > 500){
-    v = v * -1;
+    vx = vx * -1;
+  }
+
+  if(y <= 0 || y > 500){
+    vy = vy * -1;
   }
  
-  if(y <= 0 || x > 400){
-    v = v * -1;
-  }
 }
+
+
